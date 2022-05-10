ae6672d7bdfa43d6daf50ae60dcbeb874ff5b0eb
nole2701 <nole2701@gmail.com>
2022-05-10 14:18:37 -0400

    Finished day 1 practice activity 10-May-2022

--- app/src/main/java/com/example/in_class_demo/Practice.java
+++ app/src/main/java/com/example/in_class_demo/Practice.java
@@ -2,7 +2,12 @@ package com.example.in_class_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Practice extends AppCompatActivity {
 
@@ -11,5 +16,23 @@ public class Practice extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
         setTitle("Practice In Class");
+
+        Button buttonLogcat = findViewById(R.id.buttonLogcat);
+        buttonLogcat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Activity population...
+                Log.d("demo", "Practice Practice Practice!!!");
+            }
+        });
+
+        Button buttonToast = findViewById(R.id.buttonToast);
+        buttonToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Activity population...
+                Toast.makeText(Practice.this, "Now push to GitHub and Submit!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
