b647367128d2b5be8145279de70dd81275dc2445
nole2701 <nole2701@gmail.com>
2022-05-10 14:08:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/in_class_demo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.in_class_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final String TAG1 = "demo";
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG1, "Hello World");
+        Toast.makeText(this, "Showing Demo Toast!", Toast.LENGTH_LONG).show();
+        Log.d(TAG1, "toast shown");
+
+        Button buttonPractice = findViewById(R.id.buttonPractice);
+        buttonPractice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Activity population...
+                Intent toPractice = new Intent(MainActivity.this, Practice.class);
+                startActivity(toPractice);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/in_class_demo/Practice.java
@@ -0,0 +1,15 @@
+package com.example.in_class_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Practice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+        setTitle("Practice In Class");
+    }
+}
