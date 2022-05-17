969227348697ab3bb2240f677cb6fd00da82f75f
nole2701 <nole2701@gmail.com>
2022-05-17 03:52:55 -0400

    did design for inclass02

---
+++ app/src/main/java/com/example/in_class_demo/selectAvatar.java
@@ -0,0 +1,14 @@
+package com.example.in_class_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selectAvatar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_avatar);
+    }
+}
